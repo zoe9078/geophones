930bf5c5aaefd5e5ee90c72ffc714105293902e3
Zoe White <59811778+zoe9078@users.noreply.github.com>
2023-02-17 11:16:41 -0500

    Create rotation.js

---
+++ rotation.js
@@ -0,0 +1,9 @@
+ handleRotation(event) {
+    if (isMarkerVisible) {
+      el.object3D.rotation.y +=
+        event.detail.positionChange.x * rotationFactor;
+
+      el.object3D.rotation.x +=
+        event.detail.positionChange.y * rotationFactor;
+    }
+  }
